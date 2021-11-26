09068829c65edfd0026f214e8236a405ff79532e
Joshua Weinstein <jweinstein@splunk.com>
2021-11-26 15:18:23 -0800

    avx and avx2 checking

--- setup.py
+++ setup.py
@@ -96,6 +96,36 @@ with CheckCCompiles("ssse3", x86_header_string + """
     if DEFAULT_COMPILER == 'unix':
       compiler_flags.append('-mssse3')
 
+with CheckCCompiles("avx", x86_header_string + """
+
+int main(void) {
+    __m256d a = _mm256_set1_pd(3.0);
+    __m256d b = _mm256_set1_pd(3.0);
+    __m256d added =  _mm256_add_pd(a, b);
+    (void)added;
+    return 0;
+} 
+""") as avx_test:
+  if avx_test.works:
+    macro_defs.append(('PYSIMD_X86_AVX', '1'))
+    if DEFAULT_COMPILER == 'unix':
+      compiler_flags.append('-mavx')
+
+with CheckCCompiles("avx2", x86_header_string + """
+
+int main(void) {
+    __m256i a = _mm256_set1_epi32(-20);
+    __m256i abs_a = _mm256_abs_epi16(a);
+    __m256i and_a = _mm256_and_si256(a, abs_a);
+    (void)and_a;
+    return 0;
+} 
+""") as avx2_test:
+  if avx2_test.works:
+    macro_defs.append(('PYSIMD_X86_AVX2', '1'))
+    if DEFAULT_COMPILER == 'unix':
+      compiler_flags.append('-mavx2')
+
 with CheckCCompiles("avx512f", x86_header_string + """
 
 #include <stdio.h>
@@ -109,8 +139,8 @@ int main(void) {
     _mm512_store_si512((void*)storedata, added);
     return 0;
 } 
-""") as ssse3_test:
-  if ssse3_test.works:
+""") as avx512f_test:
+  if avx512f_test.works:
     macro_defs.append(('PYSIMD_X86_AVX512F', '1'))
     if DEFAULT_COMPILER == 'unix':
       compiler_flags.append('-mavx512f')
